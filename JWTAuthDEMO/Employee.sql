1334fbf3eb8eca5e4e228ef6ce335d88994a481f
Claes Engelin <claes.engelin@zocom.se>
2022-06-01 09:21:39 +0200

    Add project files.

---
+++ JWTAuthDEMO/Employee.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Employee](
+	[EmployeeID] [int] NOT NULL,
+	[NationalIDNumber] [nvarchar](15) NOT NULL,
+	[EmployeeName] [nvarchar](100) NULL,
+	[LoginID] [nvarchar](256) NOT NULL,
+	[JobTitle] [nvarchar](50) NOT NULL,
+	[BirthDate] [date] NOT NULL,
+	[MaritalStatus] [nchar](1) NOT NULL,
+	[Gender] [nchar](1) NOT NULL,
+	[HireDate] [date] NOT NULL,
+	[VacationHours] [smallint] NOT NULL,
+	[SickLeaveHours] [smallint] NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+	CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED
+	(
+		[EmployeeID] ASC
+	)
+)
---
+++ JWTAuthDEMO/UserInfo.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserInfo](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[DisplayName] [varchar](60) NOT NULL,
+	[UserName] [varchar](30) NOT NULL,
+	[Email] [varchar](50) NOT NULL,
+	[Password] [varchar](20) NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	CONSTRAINT [PK_UserInfo] PRIMARY KEY CLUSTERED
+	(
+		[UserId] ASC
+	)
+)
